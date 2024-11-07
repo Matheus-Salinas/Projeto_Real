dd37a908cd7a071d7a4fc28bc6b5a537e778319a
Matheus Salinas Zancopé <146689163+Matheus-Salinas@users.noreply.github.com>
2024-11-07 16:18:14 -0300

    Delete teste directory

--- teste/action.js
+++
@@ -1,8 +0,0 @@
-// JavaScript para dispositivos móveis
-document.querySelectorAll('.pagina1, .pagina2, .pagina3, .pagina4').forEach(item => {
-    item.addEventListener('click', () => {
-        const descricao = item.querySelector('.descricao, .descricao2, .descricao3, .descricao4');
-        descricao.classList.toggle('show'); // Alterna a visibilidade
-    });
-});
-
